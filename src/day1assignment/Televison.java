ecea028db59d0eaacc8a8a4bdeed4cb82d49c0a2
ojaswee <thapaojaswee01@gmail.com>
2023-04-14 21:35:19 -0400

    day1assignment

---
+++ src/day1assignment/Televison.java
@@ -0,0 +1,15 @@
+package day1assignment;
+
+public class Televison {
+
+     /* STATES
+     -> screen size
+     -> Brand
+     -> Resolution
+     -> Display Technology
+     ->  Color
+     ->  model name
+     ->  Product Dimensions
+
+     */
+}
