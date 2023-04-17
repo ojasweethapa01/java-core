c11183a3d2e8fad16098167e0e882e50c39c2b35
ojaswee <thapaojaswee01@gmail.com>
2023-04-16 20:32:38 -0400

    some ecommerce classes and states identified

---
+++ src/day2/Vendor.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Vendor {
+    /*
+        Vendor Id
+        Vendor Name
+        Vendor Email
+        Password
+        Store Name
+        Store Location
+        Positive Seller Rating
+        Ship on time
+        Chat Response
+     */
+}
