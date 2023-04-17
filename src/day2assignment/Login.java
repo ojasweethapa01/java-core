36bd416f70ae68787a8bd753e77c14e47febfa79
ojaswee <thapaojaswee01@gmail.com>
2023-04-16 21:01:05 -0400

    some Ecommerce classes and states identified

---
+++ src/day2assignment/Login.java
@@ -0,0 +1,13 @@
+package day2assignment;
+
+public class Login {
+    /*
+         location
+        CUSTOMER phone number  or
+        customer email id
+        password
+        login  with facebook or
+        login with gmail id
+
+     */
+}
