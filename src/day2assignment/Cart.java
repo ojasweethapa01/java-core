44b8a5f0e00ad5545f1deaa15d2db6db5c6704b4
ojaswee <thapaojaswee01@gmail.com>
2023-04-16 21:25:30 -0400

    some ecommerce classes and states identified

---
+++ src/day2assignment/Cart.java
@@ -0,0 +1,17 @@
+package day2assignment;
+
+public class Cart {
+    /*
+        select the item
+        product details
+        quantity : 3
+        size of the product (0.5-Ounce )
+        price of the product
+        delete the product
+        save for later
+        share the link
+        products related to items in your cart
+        proceed to checkouts
+
+     */
+}
--- src/day2assignment/Login.java
+++ src/day2assignment/Login.java
@@ -3,8 +3,7 @@ package day2assignment;
 public class Login {
     /*
          location
-        CUSTOMER phone number  or
-        customer email id
+        CUSTOMER phone number  or customer email id
         password
         login  with facebook or
         login with gmail id
