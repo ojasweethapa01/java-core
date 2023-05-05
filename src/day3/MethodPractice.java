db7c1df06700bf00143c6662414491bb242178ad
ojaswee <thapaojaswee01@gmail.com>
2023-05-04 23:00:54 -0400

    day3 updated

--- src/day3/MethodPractice.java
+++ src/day3/MethodPractice.java
@@ -8,24 +8,32 @@ public class MethodPractice {
         MethodPractice methodPractice = new MethodPractice();
         int a = 2;
         int b = 3;
-         int sum1 = a + b;
-            //System.out.println(sum1);
-             methodPractice.addPrint(a,b);
+         // int sum1 = a + b;
+         //System.out.println(sum1);
+            // methodPractice.addPrint(a,b);
+            int sum1 = methodPractice.addReturn(a,b);
+         System.out.println(sum1);
 
         int c = 2;
         int d = 8;
-        int sum2 = c + d;
-
-           //System.out.println(sum2);
-           methodPractice.addPrint(c,d);
+        //int sum2 = c + d;
+        //System.out.println(sum2);
+           //methodPractice.addPrint(c,d);
+        int sum2 = methodPractice.addReturn(c,d);
+        System.out.println(sum2);
 
     }
 
-    // a function that doesnot return anything - void
-
+    // a function that doesn't return anything - void
     public void addPrint(int a , int b){
             int sum = a + b;
             System.out.println(sum);
+    }
+
+    public int addReturn(int a, int b){
+        //int sum = a+b;
+        //return sum ;
+        return a + b;
     }
 
 }
